ea9688a24922ddaad70ebede4df500abab641635
smaxwell <smaxwell@SMAXWELLSP3.bayviewglen.ca>
2016-09-13 11:34:48 -0400

    Intitial Commit

---
+++ UnitOne/src/com/bayviewglen/dayfour/ExampleOne.java
@@ -0,0 +1,33 @@
+/*
+ * Integer Division and the Modulus
+ */
+
+package com.bayviewglen.dayfour;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		System.out.println(3*6);	// 18
+		System.out.println(3+6);	// 9
+		System.out.println(3/6);	// 0 - both operands are integers
+		
+		System.out.println(3.0*6);	// 18.0
+		System.out.println(3+6.0);	// 9.0
+		System.out.println(3.0/6);	// 0.5 - one operand is a double so I get double division
+		
+		System.out.println(3%6);	// 33 - the remainder is 3
+		System.out.println(23%2);	// 1 - this is how we can find out if it is an even or an odd
+		System.out.println(458548%10);	// 8 mod it by 10 to get the last digit
+		
+
+		int num = 74325;
+		int digit1 = num / 10000;
+		int digit5 = num % 10;
+		int digit4 = num % 100 / 10;
+		int digit3 = num % 1000 / 100;
+		int digit2 = num % 10000 / 1000;
+		int sum = digit1 + digit2 + digit3 + digit4 + digit5;
+		
+		
+	}
+}
