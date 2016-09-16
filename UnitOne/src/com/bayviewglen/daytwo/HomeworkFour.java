8c3ae4e7ac1a779e7a440368bf2f6b86b099fb69
smaxwell <smaxwell@SMAXWELLSP3.bayviewglen.ca>
2016-09-16 13:59:22 -0400

    Initial Commit

--- UnitOne/src/com/bayviewglen/daytwo/HomeworkFour.java
+++ UnitOne/src/com/bayviewglen/daytwo/HomeworkFour.java
@@ -4,7 +4,15 @@ public class HomeworkFour {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("/////////////////////////////////////////////////////////////////////");
+System.out.println("/                                                                   /");
+System.out.println("/                      Our Lady of Mercy Hospital                   /");
+System.out.println("/                -------------------------------------              /");
+System.out.println("/                                                                   /");
+System.out.println("/                         1306 Central City                         /");
+System.out.println("/                        Phone (416) 555-1212                       /");
+System.out.println("/                                                                   /");
+System.out.println("/////////////////////////////////////////////////////////////////////");
 	}
 
 }
--- UnitOne/src/com/bayviewglen/daytwo/HomeworkThree.java
+++ UnitOne/src/com/bayviewglen/daytwo/HomeworkThree.java
@@ -4,7 +4,9 @@ public class HomeworkThree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Sydney Maxwell");
+System.out.println("124 Ridley Boulevard");
+System.out.println("Toronto,ON M5M 3L9");
 	}
 
 }
