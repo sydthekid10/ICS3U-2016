bdffbc0ab4e645e3bf3f0bf6aaa2d6726ff1014e
smaxwell <smaxwell@smaxwellsp3.bayviewglen.ca>
2016-09-19 11:44:18 -0400

    Initial Commit

---
+++ UnitOne/src/com/bayviewglen/daysix/HomeworkEight.java
@@ -0,0 +1,11 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkEight {
+
+	public static void main(String[] args) {
+		
+		Scanner ew = new Scanner(System.in);
+		
+	}
+
+}
--- UnitOne/src/com/bayviewglen/daysix/HomeworkSeven.java
+++ UnitOne/src/com/bayviewglen/daysix/HomeworkSeven.java
@@ -4,7 +4,10 @@ public class HomeworkSeven {
 
 	public static void main(String[] args) {
 		
+		double farenheit = 98.0;
+		double celcius = farenheit / 1.8 - 32;
 		
+		System.out.println("98 degrees Farenheit is " + celcius + " degrees Celsius.");
 
 	}
 
