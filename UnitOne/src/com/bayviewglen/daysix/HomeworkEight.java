52c7a6e0afc9528ff79e90853f0e5884d5dba258
smaxwell <smaxwell@smaxwellsp3.bayviewglen.ca>
2016-09-20 22:32:14 -0400

    Initial Commit

--- UnitOne/src/com/bayviewglen/daysix/HomeworkEight.java
+++ UnitOne/src/com/bayviewglen/daysix/HomeworkEight.java
@@ -1,11 +1,15 @@
 package com.bayviewglen.daysix;
 
+import java.util.Scanner;
+
 public class HomeworkEight {
 
 	public static void main(String[] args) {
 		
-		Scanner ew = new Scanner(System.in);
+		Scanner dolphin = new Scanner(System.in);
 		
+		double positiveNumber = dolphin.nextDouble();
+		System.out.print("Enter a postive number:");
 	}
 
 }
